4f144b68c17493c9a7e97196c8e14c29b7cd75dd
Wolfgang Rönninger <wroennin@student.ethz.ch>
2019-10-09 11:25:57 +0200

    added BYTE_WIDTH to parameters

--- src/sram.sv
+++ src/sram.sv
@@ -12,35 +12,48 @@
 // Description: SRAM Behavioral Model
 
 module sram #(
-    int unsigned DATA_WIDTH = 64,
-    int unsigned NUM_WORDS  = 1024
-)(
-   input  logic                          clk_i,
+  parameter int unsigned DATA_WIDTH = 64,
+  parameter int unsigned BYTE_WIDTH = 1,
+  parameter int unsigned NUM_WORDS  = 1024,
+  // Dependent parameters, do not overwrite!
+  parameter int unsigned BE_WIDTH   = cf_math_pkg::ceil_div(DATA_WIDTH, BYTE_WIDTH),
+  parameter type         addr_t     = logic[$clog2(NUM_WORDS)-1:0],
+  parameter type         data_t     = logic[DATA_WIDTH-1:0],
+  parameter type         strb_t     = logic[BE_WIDTH-1:0]
+) (
+  input  logic  clk_i,
 
-   input  logic                          req_i,
-   input  logic                          we_i,
-   input  logic [$clog2(NUM_WORDS)-1:0]  addr_i,
-   input  logic [DATA_WIDTH-1:0]         wdata_i,
-   input  logic [DATA_WIDTH-1:0]         be_i,
-   output logic [DATA_WIDTH-1:0]         rdata_o
+  input  logic  req_i,
+  input  logic  we_i,
+  input  addr_t addr_i,
+  input  data_t wdata_i,
+  input  strb_t be_i,
+  output data_t rdata_o
 );
-    localparam ADDR_WIDTH = $clog2(NUM_WORDS);
+  data_t ram [NUM_WORDS-1:0];
+  addr_t raddr_q;
 
-    logic [DATA_WIDTH-1:0] ram [NUM_WORDS-1:0];
-    logic [ADDR_WIDTH-1:0] raddr_q;
-
-    // 1. randomize array
-    // 2. randomize output when no request is active
-    always_ff @(posedge clk_i) begin
-        if (req_i) begin
-            if (!we_i)
-                raddr_q <= addr_i;
-            else
-            for (int i = 0; i < DATA_WIDTH; i++)
-                if (be_i[i]) ram[addr_i][i] <= wdata_i[i];
+  // 1. randomize array
+  // 2. randomize output when no request is active
+  always_ff @(posedge clk_i) begin
+    if (req_i) begin
+      if (!we_i) begin
+        raddr_q <= addr_i;
+      end else begin
+        for (int unsigned i = 0; i < BE_WIDTH; i++) begin
+          if (be_i[i]) begin
+            ram[addr_i][i*BYTE_WIDTH:BYTE_WIDTH] <= wdata_i[i*BYTE_WIDTH:BYTE_WIDTH];
+          end
         end
+      end
     end
+  end
 
-    assign rdata_o = ram[raddr_q];
+  assign rdata_o = ram[raddr_q];
 
+  initial begin : proc_check_params
+    sram_byte_width : assume (DATA_WIDTH % BYTE_WIDTH == 0) else
+      $warning($sformatf("sram > `DATA_WIDTH`: %0d is not a multiple of `BYTE_WIDTH`: %0d",
+          DATA_WIDTH, BYTE_WIDTH));
+  end
 endmodule
